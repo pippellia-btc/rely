1566bf278a0a4584fddbb61efda8d1f95b2c3a92
pippellia-btc <pippellia@gmail.com>
2025-03-26 12:52:12 +0100

    options are now clientLimits

--- pkg/ws/client.go
+++ pkg/ws/client.go
@@ -2,46 +2,15 @@ package ws
 
 import (
 	"context"
-	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/nbd-wtf/go-nostr"
-)
-
-const (
-	DefaultWriteWait      = 10 * time.Second
-	DefaultPongWait       = 60 * time.Second
-	DefaultPingPeriod     = 45 * time.Second
-	DefaultMaxMessageSize = 512000
-
-	DefaultMaxSubscriptions = 50
 )
 
 type Subscription struct {
 	ID      string
 	cancel  context.CancelFunc // calling it cancels the context associated with the REQ
 	Filters nostr.Filters
-}
-
-type Options struct {
-	// websocket options
-	WriteWait      time.Duration
-	PongWait       time.Duration
-	PingPeriod     time.Duration
-	MaxMessageSize int
-
-	// client options
-	MaxSubscriptions int
-}
-
-func NewOptions() Options {
-	return Options{
-		WriteWait:        DefaultWriteWait,
-		PongWait:         DefaultPongWait,
-		PingPeriod:       DefaultPingPeriod,
-		MaxMessageSize:   DefaultMaxMessageSize,
-		MaxSubscriptions: DefaultMaxSubscriptions,
-	}
 }
 
 // Client is a middleman between the websocket connection and the [Relay].
@@ -52,20 +21,6 @@ type Client struct {
 	Conn *websocket.Conn
 	Send chan []byte
 	Subs []Subscription
-	Options
-}
-
-func NewClient(conn *websocket.Conn) *Client {
-	return NewClientWithOptions(conn, NewOptions())
-}
-
-func NewClientWithOptions(conn *websocket.Conn, opt Options) *Client {
-	return &Client{
-		Conn:    conn,
-		Send:    make(chan []byte, 256),
-		Subs:    make([]Subscription, 0, 5),
-		Options: opt,
-	}
 }
 
 func (c *Client) CloseSubscription(ID string) {
--- pkg/ws/relay.go
+++ pkg/ws/relay.go
@@ -1,6 +1,18 @@
 package ws
 
-import "github.com/gorilla/websocket"
+import (
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	DefaultWriteWait                 = 10 * time.Second
+	DefaultPongWait                  = 60 * time.Second
+	DefaultPingPeriod                = 45 * time.Second
+	DefaultMaxMessageSize            = 512000
+	DefaultMaxSubscriptionsPerClient = 50
+)
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -13,13 +25,41 @@ type Relay struct {
 	Clients    map[*Client]bool
 	Register   chan *Client
 	Unregister chan *Client
+
+	ClientLimits
+}
+
+type ClientLimits struct {
+	// websocket options
+	WriteWait      time.Duration
+	PongWait       time.Duration
+	PingPeriod     time.Duration
+	MaxMessageSize int
+
+	// client options
+	MaxSubscriptionsPerClient int
+}
+
+func NewClientLimits() ClientLimits {
+	return ClientLimits{
+		WriteWait:                 DefaultWriteWait,
+		PongWait:                  DefaultPongWait,
+		PingPeriod:                DefaultPingPeriod,
+		MaxMessageSize:            DefaultMaxMessageSize,
+		MaxSubscriptionsPerClient: DefaultMaxSubscriptionsPerClient,
+	}
 }
 
 func NewRelay() *Relay {
+	return NewRelayWithOptions(NewClientLimits())
+}
+
+func NewRelayWithOptions(limits ClientLimits) *Relay {
 	return &Relay{
-		Queue:      make(chan []byte, 10000),
-		Clients:    make(map[*Client]bool, 100),
-		Register:   make(chan *Client, 10),
-		Unregister: make(chan *Client, 10),
+		Queue:        make(chan []byte, 10000),
+		Clients:      make(map[*Client]bool, 100),
+		Register:     make(chan *Client, 10),
+		Unregister:   make(chan *Client, 10),
+		ClientLimits: limits,
 	}
 }
